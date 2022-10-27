1a0a22fa9990a29776d0a75385296c38034dc976
Marinus <marinus@impactinstitute.com>
2022-10-27 17:55:06 +0200

    remove commented out code that will be implemented later

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -5,16 +5,8 @@ open System
 type ChocolateBar =
        {
         Name      : string
-        // Footprint : Footprint
         Price     : float
-        // Impact    : Impact option
-        // TruePrice : float option
        }
-// and Footprint =
-//        { CO2 : float
-//          CH4 : float }
-// and Impact =
-//        { ClimateChange : float }
 
 module ChocolateBar =
     let isValidValue (value : float) = value >= 0.0
@@ -24,11 +16,7 @@ module ChocolateBar =
 
         //return bool value indicating whether input data is valid
         (inputChocolateBar.Name |> isValidName) &&
-        // (inputChocolateBar.Footprint.CO2 |> isValidValue) &&
-        // (inputChocolateBar.Footprint.CH4 |> isValidValue) &&
         (inputChocolateBar.Price |> isValidValue)
-        // (inputChocolateBar.Impact |> Option.isNone) &&
-        // (inputChocolateBar.TruePrice |> Option.isNone)
     
     let create (name: string) (price : float) =
         {
@@ -47,7 +35,6 @@ module Route =
 
 type IChocolateBarApi =
     {
-        //calculateTruePrice : ChocolateBar -> Async<ChocolateBar>
         getChocolateBars: unit -> Async<ChocolateBar list>
         addChocolateBar: ChocolateBar -> Async<ChocolateBar>
     }
