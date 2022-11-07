9c0d67752a6e2cd87a8f6f595f55ab82e9fa630a
Marinus <marinus@impactinstitute.com>
2022-11-07 16:47:08 +0100

    Revert "initial commit Shared"
    This reverts commit ca31e226617e531475d72248faad7b89acbea194.

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -5,18 +5,28 @@ open System
 type ChocolateBar =
        {
         Name      : string
+        Price     : float
        }
 
 module ChocolateBar =
+    let isValidValue (value : float) = value >= 0.0
     let isValidName (name : string) = String.IsNullOrWhiteSpace name |> not
+
+    let isValidChocolateBar (inputChocolateBar : ChocolateBar) =
+
+        //return bool value indicating whether input data is valid
+        (inputChocolateBar.Name |> isValidName) &&
+        (inputChocolateBar.Price |> isValidValue)
     
-    let create (name: string) =
+    let create (name: string) (price : float) =
         {
             Name = name
+            Price = price
         }
     
     let createEmpty() = {
         Name = ""
+        Price = 0.0
     }
 
 module Route =
