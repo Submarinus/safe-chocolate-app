30c3766b7e78bf7b907faecf857b1a0a2772462d
Marinus <115226965+Submarinus@users.noreply.github.com>
2022-12-05 16:20:27 +0100

    Index.fs onlyname

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -7,13 +7,11 @@ open Shared
 type State = {
     ChocolateBars : ChocolateBar list
     InputName : string
-    InputValue : float
 }
 
 type Msg =
     | GotChocolateBars of ChocolateBar list
     | SetInputName of string
-    | SetInputValue of float
     | AddChocolateBar
     | AddedChocolateBar of ChocolateBar
 
@@ -26,32 +24,26 @@ let init() = {
     ChocolateBars = [
         {
             Name = "Tony's #1"
-            Price = 3.2
             }
         {
             Name = "Tony's #2"
-            Price = 3.0
             }
     ]
     InputName = ""
-    InputValue = 0.0
     }
 
 let update (msg: Msg) (state: State) : State =
     match msg with
     | GotChocolateBars chocolateBars -> { state with ChocolateBars = chocolateBars }
     | SetInputName name -> { state with InputName = name }
-    | SetInputValue value -> { state with InputValue = value }
     | AddChocolateBar ->
         let chocolateBar =
             ChocolateBar.create
                 state.InputName
-                state.InputValue
 
         { state with
             InputName = ""
-            InputValue = 0.0
-            ChocolateBars = 
+            ChocolateBars =
                 printfn "Chocolate bar added"
                 List.append state.ChocolateBars [chocolateBar]
             }
@@ -85,25 +77,6 @@ let inputName (state: State) (dispatch: Msg -> unit) =
     ]
   ]
 
-let inputPrice (state: State) (dispatch: Msg -> unit) =
-  Html.div [
-    prop.classes [ "field"; "has-addons" ]
-    prop.children [
-      Html.div [
-        prop.classes [ "control"; "is-expanded"]
-        prop.children [
-          Html.input [
-            prop.classes [ "input"; "is-medium"; "number" ]
-            prop.type'.number
-            prop.valueOrDefault state.InputValue
-            prop.placeholder "What is the price of the chocolate bar?"
-            prop.onChange (SetInputValue >> dispatch)
-          ]
-        ]
-      ]
-    ]
-  ]
-
 let createChocolateBar (state : State) (dispatch : Msg -> unit) =
       Html.div [
         prop.className "control"
@@ -127,12 +100,6 @@ let renderChocolateBar (chocolateBar : ChocolateBar) (dispatch: Msg -> unit) =
             prop.text chocolateBar.Name
             ]
         ]
-        div [ "column" ] [
-            Html.p [
-            prop.className "Price"
-            prop.text chocolateBar.Price
-            ]
-        ]
         ]
     ]
 
@@ -142,11 +109,6 @@ let chocolateBarLegend =
             div [ "column" ] [
                 Html.p [
                 prop.text "Name"
-                ]
-            ]
-            div [ "column" ] [
-                Html.p [
-                prop.text "Price"
                 ]
             ]
         ]
@@ -172,9 +134,7 @@ let render (state: State) (dispatch: Msg -> unit) =
     prop.children [
       appTitle
       inputName state dispatch
-      inputPrice state dispatch
       createChocolateBar state dispatch
-      chocolateBarLegend
       chocolateBars state dispatch
     ]
   ]
